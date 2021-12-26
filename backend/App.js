d10ffddac878041301fcf9a080faeb7e523a6dde
NeroYuki <kingrfminecraft@gmail.com>
2021-12-26 23:01:33 +0700

    some scripts

--- backend/App.js
+++ backend/App.js
@@ -1,10 +1,22 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path');
+const cors = require("cors")
 
-app.get('/', (req, res) => {
+const app = express()
+const port = 5000
+
+app.use(cors({
+    origin: ['https://kansenindex.xyz', 'https://www.kansenindex.xyz']
+}));
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
+
+app.get('/api/test', (req, res) => {
     res.send('Hello World! From neroyuki\'s droplet')
 })
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/../frontend/build/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Application listening at http://localhost:${port}`)
