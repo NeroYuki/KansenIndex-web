c1297c73243e29b3e5117ea78b9f1346b8adc843
NeroYuki <neroyuki241@gmail.com>
2024-07-08 01:19:47 +0700

    urgent reponsive fix

--- frontend/src/App/Component/SiteHeader/index.js
+++ frontend/src/App/Component/SiteHeader/index.js
@@ -16,7 +16,7 @@ export const SiteHeader = (props) => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const { colorMode, toggleColorMode } = useColorMode()
     return (
-        <Flex direction={'column'} style={{position: 'fixed', zIndex: 4, width: '100%', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
+        <Flex direction={'column'} style={{position: 'fixed', zIndex: 4, width: '100vw', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
             <Box flex='1' 
                 bg="primary" 
                 p='20px'>
